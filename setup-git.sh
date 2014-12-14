2ec4a952b7f84c17f6d708e9a9540fc6b4a118a0
Pavel Novy <feed57005@gmail.com>
2014-12-14 01:22:48 +0100

    git setup

---
+++ setup-git.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if ! type git &> /dev/null; then
+	echo "install git"
+	exit -1
+fi
+
+read -p "git user.name: "
+echo
+git config --global user.name "$REPLY"
+
+echo
+read -p "git user.email: "
+git config --global user.email "$REPLY"
+
+# [alias]
+git config --global alias.nl "log --stat"
+git config --global alias.ls "status"
+git config --global alias.sls "stash list --name-status"
+git config --global alias.b "branch --color -vv -a --abbrev=5"
+
+# [color]
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
+git config --global color.stash auto
--- setup.sh
+++ setup.sh
@@ -28,18 +28,28 @@ ln -s $script_path/.inputrc ~/.inputrc
 ln -s $script_path/.tmux.conf ~/.tmux.conf
 ln -s $script_path/.screenrc ~/.screenrc
 
+read -p "Generate ssh key? (y/n): " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	./gen-ssh.sh id_rsa
+fi
+
+read -p "Setup git? (y/n): " -n 1 -r
+echo
+if type git &> /dev/null; then
+	./setup-git.sh
+else
+	echo "git is not installed!"
+	exit -1
+fi
+
 if type vim &> /dev/null; then
-	read -p "setup vim? (y/n/q)" -n 1 -r
+	read -p "Setup vim? (y/n): " -n 1 -r
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		./install-vimrc.sh
+		./setup-vim.sh
 	fi
 else
 	echo "vim not installed, run ./install-vimrc.sh manually after installation"
 fi
 
-read -p "generate ssh key? (y/n/q)" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	./gen-ssh.sh id_rsa
-fi
