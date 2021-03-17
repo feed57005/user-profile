69a8ff79c88439b06d4d5673b8abc193bc7283c2
Pavel Novy <feed57005@gmail.com>
2021-03-18 00:52:32 +0100

    setup fonts + essentail debian/ubuntu packages

---
+++ setup-fonts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ~/.fonts
+
+curl -L https://github.com/powerline/fonts/raw/master/LiberationMono/Literation%20Mono%20Powerline%20Bold%20Italic.ttf -o ~/.fonts/Literation_Mono_Powerline_Bold_Italic.ttf
+curl -L https://github.com/powerline/fonts/raw/master/LiberationMono/Literation%20Mono%20Powerline%20Bold.ttf -o ~/.fonts/Literation_Mono_Powerline_Bold.ttf
+curl -L https://github.com/powerline/fonts/raw/master/LiberationMono/Literation%20Mono%20Powerline%20Italic.ttf -o ~/.fonts/Literation_Mono_Powerline_Italic.ttf
+curl -L https://github.com/powerline/fonts/raw/master/LiberationMono/Literation%20Mono%20Powerline.ttf -o ~/.fonts/Literation_Mono_Powerline.ttf
